f5938bd3ce6cbc3b6cbac236dac08c51656c06c5
khadim Raath <khadim.raath@incubasys.com>
2015-08-26 00:05:59 +0500

    Aws keys are hidden

---
+++ aws_keys.php
@@ -0,0 +1,9 @@
+<?php 
+
+function aws_keys(){
+	$access_key='AKIAJCE4ZDY4NPCMCZMA';
+	$secret_key='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';
+	return array("access_key"=>$access_key,"secret_key"=>$secret_key);
+}
+
+?>
